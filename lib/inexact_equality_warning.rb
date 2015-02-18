a114b04293a60f14dabf8554173c880156aab845
Adam Prescott <adam@aprescott.com>
2015-02-17 19:49:49 -0500

    Now with even more tests.

--- lib/inexact_equality_warning.rb
+++ lib/inexact_equality_warning.rb
@@ -1,7 +1,7 @@
 # it would be good to use #inexact? here -- https://bugs.ruby-lang.org/issues/5321
 module EqualityWarning
   def ==(other)
-    warn "WARNING: Testing for equality between inexact floats is ill-advised, when comparing #{self} and #{other} (#{caller.first})" if self.is_a?(Float) || other.is_a?(Float)
+    warn "WARNING: Testing for equality between inexact floats is ill-advised, when comparing #{self} and #{other} (#{caller.first})" if (self.is_a?(Numeric) && other.is_a?(Numeric)) && self.is_a?(Float) || other.is_a?(Float)
     super
   end
 end
