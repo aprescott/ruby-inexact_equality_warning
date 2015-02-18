bc11f827c89bfa65530392ad9e5f5319f734f2e7
Adam Prescott <adam@aprescott.com>
2015-02-17 22:12:54 -0500

    'with' makes more sense than 'between' since only one float is needed.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ```ruby
 x = 1.0 - 0.9 - 0.1
 x == 0.0
-# WARNING: Testing for equality between inexact floats is ill-advised, when comparing -2.7755575615628914e-17 and 0 (/path/to/file.rb:123)
+# WARNING: Testing for equality with inexact floats is ill-advised, when comparing -2.7755575615628914e-17 and 0 (/path/to/file.rb:123)
 # => false
 ```
 
--- spec/inexact_equality_warning_spec.rb
+++ spec/inexact_equality_warning_spec.rb
@@ -11,7 +11,7 @@ end
 
 RSpec.describe EqualityWarning do
   def expect_warning(x, y)
-    expect(capturing_stderr { x == y }).to include("WARNING: Testing for equality between inexact floats is ill-advised, when comparing #{x} and #{y} (#{__FILE__}:#{__LINE__}:in `block in #{__method__}'")
+    expect(capturing_stderr { x == y }).to include("WARNING: Testing for equality with inexact floats is ill-advised, when comparing #{x} and #{y} (#{__FILE__}:#{__LINE__}:in `block in #{__method__}'")
   end
 
   def expect_no_warning(x, y)
